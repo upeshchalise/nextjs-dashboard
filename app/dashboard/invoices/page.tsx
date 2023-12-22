1a2cc022c990783e54b6b1e7333cc98382725116
upesh chalise <upeshchalise@gmail.com>
2023-12-22 13:59:56 +0545

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoices = () => {
+  return (
+    <p>Invoices page</p>
+  )
+}
+
+export default Invoices
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const Dashboard = () => {
+  return (
+    <p>Dashboard Page</p>
+  )
+}
+
+export default Dashboard
